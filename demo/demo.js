6db480b034f1b61310d653106d9d7b1b394f66c3
yanlu <1280065541@qq.com>
2020-06-02 17:48:10 +0800

    新增获取当前周一和周日的方法

--- demo/demo.js
+++ demo/demo.js
@@ -1,24 +1,50 @@
 var UPLOAD_FILE = new Vue({
     el: '#demo',
     data: {
-        social: ['twitter'],
-        selectArr: [
-            { name: '111', value: 0 },
-            { name: '222', value: 1 }
-        ],
-        tempSelect: 0
+        // dates: []
+        a: 12345
     },
     methods: {
-        selectChange: function () {
-            console.log(this.tempSelect)
+        test: function (date1, date2) {
         }
-    }
+    },
 })
 
-var b = 3;
-(function () {
-    b = 5;
-    var b = 2;
-    console.log(this)
-})();
-console.log(b);
+
+/** 
+ * @param {*} time 时间字符串（2020-06-02）
+ * @param {*} type 获取周一（start）还是周日（end）
+ */
+function getWeekEndDay (time, type) {
+    if (time == '' || type == '')
+        return null
+    var date = new Date(time)
+    var day = date.getDay()
+    switch (type) {
+        case 'start':
+            if (day == 1) {
+                return date
+            }
+            else {
+                //获取需要减去的天数
+                day = day == 0 ? 6 : (day - 1)
+                return new Date(date.getTime() - day * 60 * 60 * 24 * 1000)
+            }
+        case 'end':
+            if (day == 0) {
+                return date
+            }
+            else {
+                day = 7 - day
+                return new Date(date.getTime() + day * 60 * 60 * 24 * 1000)
+            }
+        default:
+            return null
+    }
+}
+
+function getTimeParams (time, type) {
+
+}
+
+// console.log(getWeekEndDay('2020-06-01', 'start').toDateString())
